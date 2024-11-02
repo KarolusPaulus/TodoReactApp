8cbabf2533f1cc105f09086feab68e9b75997c01
KarolusPaulus <karosaarinenn@gmail.com>
2024-11-02 14:53:39 +0200

    everything

---
+++ src/components/Row.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useUser } from '../context/useUser'
+
+export default function ProtectedRoute() {
+    const { user } = useUser()
+    if (!user || !user.token)  return <Navigate to="/signin" />
+    return <Outlet />
+}
+
+export default function Row({item, deleteTask}) {
+    return (
+       <ul>
+        <li key = {item.id}>{item.decription}
+            <button className = 'delete-button' onClick = {() => deleteTask(item.id)}>Delete</button>
+        </li>
+        {
+            tasks.map(item => (
+                <Row key = {item.id} item = {item} deleteTask = {deleteTask}/>
+            ))
+        }
+        </ul>
+    )
+}
