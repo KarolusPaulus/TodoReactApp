371c778ec6e5a989c4ff55b7ea4258cdee3ab39b
KarolusPaulus <karosaarinenn@gmail.com>
2024-11-05 09:26:07 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
     element: <ProtectedRoute />,
     children: [
       {
-        path: "/signin",
+        path: "/",
         element: <Home />,
       }
     ]
